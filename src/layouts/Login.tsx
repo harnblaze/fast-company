8492317feaf5a887d4b6c5a9c4d07f6365b2dd25
Dmitriy Emelyanov <harnblaze@gmail.com>
2023-01-13 11:03:11 +0300

    add email and password state in login layout

--- src/layouts/Login.tsx
+++ src/layouts/Login.tsx
@@ -1,15 +1,33 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 const Login: FC = () => {
+  const [data, setData] = useState({ email: "", password: "" });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setData((prev) => ({ ...prev, [event.target.name]: event.target.value }));
+  };
+
   return (
     <form action="">
       <div>
         <label htmlFor="email">Email</label>
-        <input type="text" id="email" />
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={data.email}
+          onChange={handleChange}
+        />
       </div>
       <div>
         <label htmlFor="password">Пароль</label>
-        <input type="password" id="password" />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={data.password}
+          onChange={handleChange}
+        />
       </div>
     </form>
   );
