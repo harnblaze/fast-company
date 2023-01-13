56647b0dce1fc2af2b401f52e1553582a5861ce0
Dmitriy Emelyanov <harnblaze@gmail.com>
2023-01-13 12:45:04 +0300

    add base validation

--- src/layouts/Login.tsx
+++ src/layouts/Login.tsx
@@ -1,15 +1,35 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import TextField from "../components/TextField";
 
 const Login: FC = () => {
   const [data, setData] = useState({ email: "", password: "" });
+  const [, setErrors] = useState({ email: "", password: "" });
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setData((prev) => ({ ...prev, [event.target.name]: event.target.value }));
   };
 
+  useEffect(() => {
+    validate();
+  }, [data]);
+
+  const validate = (): boolean => {
+    const errors = { email: "", password: "" };
+    for (const fieldName in data) {
+      if (data[fieldName as keyof typeof data].trim() === "") {
+        errors[
+          fieldName as keyof typeof data
+        ] = `${fieldName} обязательно для заполнения`;
+      }
+    }
+    setErrors(errors);
+    return Object.values(errors).every((el) => el === "");
+  };
+
   const handleSubmit = (event: React.FormEvent): void => {
     event.preventDefault();
+    const isValid = validate();
+    if (!isValid) return;
     console.log(data);
   };
 
