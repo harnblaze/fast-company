d444149dad1d0e120a3d3c757a766140d0498ece
Dmitriy Emelyanov <harnblaze@gmail.com>
2023-01-15 09:48:48 +0300

    add show/hide password

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 interface TextFieldProps {
   label: string;
@@ -17,22 +17,38 @@ const TextField: FC<TextFieldProps> = ({
   onChange,
   error,
 }) => {
+  const [showPassword, setShowPassword] = useState(false);
   const getInputClasses = (): string => {
     return "form-control" + (error !== "" ? " is-invalid" : "");
+  };
+  const toggleShowPassword = (): void => {
+    setShowPassword((prevState) => !prevState);
   };
 
   return (
     <div className="mb-4">
       <label htmlFor={name}>{label}</label>
-      <input
-        type={type}
-        id={name}
-        name={name}
-        value={value}
-        onChange={onChange}
-        className={getInputClasses()}
-      />
-      <div className="invalid-feedback">{error}</div>
+      <div className="input-group has-validation">
+        <input
+          type={showPassword ? "text" : type}
+          id={name}
+          name={name}
+          value={value}
+          onChange={onChange}
+          className={getInputClasses()}
+        />
+        {type === "password" && (
+          <button
+            className="btn btn-outline-secondary"
+            type="button"
+            onClick={toggleShowPassword}
+          >
+            <i className={"bi bi-eye" + (showPassword ? "-slash" : "")}></i>
+          </button>
+        )}
+
+        <div className="invalid-feedback">{error}</div>
+      </div>
     </div>
   );
 };
